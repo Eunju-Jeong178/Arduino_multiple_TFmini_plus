49c4d81ca7317a4a900fd575bc3ff1928e850318
Eunju Jeong <77608922+Eunju-Jeong178@users.noreply.github.com>
2022-12-15 13:40:45 +0900

    Update two_TFMP.ino

--- two_TFMP.ino
+++ two_TFMP.ino
@@ -1,14 +1,11 @@
-# Four TFmini Plus LiDAR 
-# data time --> distance1 strenght1 distance2 strenght2 distance3 strenght3 distance4 strenght4
+// Two TFmini Plus LiDAR 
+// data time --> distance1 strenght1 distance2 strenght2
 
 #include <SoftwareSerial.h>  
 
 //SoftwareSerial port(TX, RX);
 SoftwareSerial portOne(2, 2);
 SoftwareSerial portTwo(3, 3); 
-SoftwareSerial portThree(8, 7);
-SoftwareSerial portFour(10, 11); 
-
 
 void getTFminiData(SoftwareSerial* port, int* distance, int* strength, boolean* complete) {
   static char i = 0;
@@ -43,8 +40,6 @@ void setup() {
   Serial.begin(115200);
   portOne.begin(115200);
   portTwo.begin(115200);
-  portThree.begin(115200);
-  portFour.begin(115200);
 }
 
 void loop() {
@@ -55,14 +50,6 @@ void loop() {
   int distance2 = 0;
   int strength2 = 0;
   boolean receiveComplete2 = false;
-
-  int distance3 = 0;
-  int strength3 = 0;
-  boolean receiveComplete3 = false;
-
-  int distance4 = 0;
-  int strength4 = 0;
-  boolean receiveComplete4 = false;
 
   while(!receiveComplete1) {
     getTFminiData(&portOne, &distance1, &strength1, &receiveComplete1);
@@ -82,34 +69,8 @@ void loop() {
       Serial.print(distance2);
       Serial.print("cm\t");
       Serial.print("strength2: ");
-      Serial.print(strength2);
-      Serial.print("\t")
+      Serial.println(strength2);
     }
   }
   receiveComplete2 = false;
-
-  while(!receiveComplete3) {
-    getTFminiData(&portThree, &distance3, &strength3, &receiveComplete3);
-    if(receiveComplete3) {
-      Serial.print(distance3);
-      Serial.print("cm\t");
-      Serial.print("strength3: ");
-      Serial.print(strength3);
-      Serial.print("\t");
-    }
-  }
-  receiveComplete3 = false;
-
-  while(!receiveComplete4) {
-    getTFminiData(&portFour, &distance4, &strength4, &receiveComplete4);
-    if(receiveComplete4) {
-      Serial.print(distance4);
-      Serial.print("cm\t");
-      Serial.print("strength4: ");
-      Serial.println(strength4);
-    }
-  }
-  receiveComplete4 = false; 
-
-
 }
